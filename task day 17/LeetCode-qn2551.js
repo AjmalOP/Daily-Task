af5a7784566e730b6c419e76e2a4b0249ac8ac66
AjmalOP <ajmalop22@gmail.com>
2024-01-22 13:07:56 +0530

    task day 17 4th

--- task day 17/LeetCode-qn2551.js
+++ task day 17/LeetCode-qn2551.js
@@ -1,1 +1,13 @@
 // LEET CODE PROBLEM 2551 : https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/
+
+var captureForts = function (forts, x = 0, j = 0) {
+  for (i = 0; i < forts.length; i++) {
+    if (forts[i]) {
+      if (forts[i] === -forts[j]) {
+        x = Math.max(x, i - j - 1);
+      }
+      j = i;
+    }
+  }
+  return x;
+};
