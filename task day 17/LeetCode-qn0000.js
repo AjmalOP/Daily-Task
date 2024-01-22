9c7407cb25a579e1383d0912efc7b0b64bbe452f
AjmalOP <ajmalop22@gmail.com>
2024-01-22 13:06:20 +0530

    task day 17 2nd

--- task day 17/LeetCode-qn0000.js
+++ task day 17/LeetCode-qn0000.js
@@ -1,1 +1,1 @@
-// LEET CODE PROBLEM 0000 : https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/
+// LEET CODE PROBLEM 2551 : https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/
