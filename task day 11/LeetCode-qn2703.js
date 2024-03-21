605665047da3d60fc2de5a59189a7d0376defa9d
amala <amala@AJMAL>
2024-03-21 11:43:06 +0530

    Push all

--- task day 11/LeetCode-qn2703.js
+++
@@ -1,3 +0,0 @@
-// LEET CODE PROBLEM 2703 : https://leetcode.com/problems/return-length-of-arguments-passed/description/
-
-var argumentsLength = (...args) => args.length
--- task day 8/LeetCode-qn2619.js
+++
@@ -1,5 +0,0 @@
-// LEET CODE PROBLEM 2665 : https://leetcode.com/problems/array-prototype-last/
-
-Array.prototype.last = function() {
-    return this.length? this[this.length-1]: -1;
-};
