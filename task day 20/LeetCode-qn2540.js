6acb790c0e288151d8117a10ca86e3f55cb99ed3
Ajmal OP <150312890+AjmalOP@users.noreply.github.com>
2024-03-23 14:30:56 +0530

    Update LeetCode-qn2540.js

--- task day 20/LeetCode-qn2540.js
+++ task day 20/LeetCode-qn2540.js
@@ -1,1 +1,22 @@
-
+ 
+var getCommon = function(nums1, nums2,i=0,k=0)
+{
+    while (i < nums1.length && k < nums2.length)
+    {
+        while (i < nums1.length && nums1[i] < nums2[k])
+        {
+            i++;
+        }
+        while (k < nums2.length && nums2[k] < nums1[i])
+        {
+            k++;
+        }
+        if (nums1[i] == nums2[k])
+        {
+            return nums1[i]
+        }
+    }
+    return -1
+};
+   
+getCommon([1,2,3], [2,4]);
