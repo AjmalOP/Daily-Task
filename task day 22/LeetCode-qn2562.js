d0dea8afece5a7ff353fba5fc85ef8f2fd170167
Ajmal OP <150312890+AjmalOP@users.noreply.github.com>
2024-03-26 10:37:37 +0530

    Update LeetCode-qn2562.js

--- task day 22/LeetCode-qn2562.js
+++ task day 22/LeetCode-qn2562.js
@@ -1,1 +1,14 @@
-
+var findTheArrayConcVal = function(nums,x=0) {
+    while(nums.length)
+    {
+        if(nums.length==1)
+        {
+            x = x+nums.shift();
+        }
+        else
+        {
+            x = x+Number(nums.shift()+""+nums.pop())
+        }
+    }
+    return x;
+};
