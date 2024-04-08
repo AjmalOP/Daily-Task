2fbd90d8ec605232a557ebab22446130d7e16fa9
Ajmal OP <150312890+AjmalOP@users.noreply.github.com>
2024-04-08 11:36:54 +0530

    Update LeetCode-qn2578.js

--- task day 23/LeetCode-qn2578.js
+++ task day 23/LeetCode-qn2578.js
@@ -1,1 +1,19 @@
 // LEET CODE PROBLEM 2578 : https://leetcode.com/problems/split-with-minimum-sum
+
+var splitNum = function(num) {
+    var sorted = num.toString().split("").sort();
+    var x = "";
+    var y = "";
+    for(i=0;i<sorted.length;i++)
+    {
+        if(i%2==0)
+        {
+            x= x+sorted[i]
+        }
+        else
+        {
+            y= y+sorted[i]
+        }
+    }
+    return Number(x)+Number(y)
+};
